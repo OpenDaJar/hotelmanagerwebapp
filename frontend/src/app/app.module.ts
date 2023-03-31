1e927a73efaa49e6cc834d9035fe4e53d017847a
Ladas Michail <47851156+OpenDaJar@users.noreply.github.com>
2023-03-31 18:24:17 +0300

    Added Navbar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/partials/header/header.component';
 
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +15,7 @@ import { HeaderComponent } from './components/partials/header/header.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,BsDropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
