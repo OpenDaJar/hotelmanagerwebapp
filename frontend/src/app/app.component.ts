f8a0030821c79d69d1b292939533d5adc98cad69
Ladas Michail <47851156+OpenDaJar@users.noreply.github.com>
2023-04-04 11:21:34 +0300

    clean up

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from './_services/auth.service';
+import { StorageService } from './_services/storage.service';
+import { Subscription } from 'rxjs';
+import { EventBusService } from './_shared/event-bus.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username!: string;
+
+  eventBusSub?: Subscription;
+
+  constructor(private storageService: StorageService, private authService: AuthService,private eventBusService: EventBusService){}
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.storageService.isLoggedIn();
+
+    if (this.isLoggedIn) {
+      const user = this.storageService.getUser();
+
+      // this.roles = user.roles;
+
+      // this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      // this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+      this.username = user.username;
+    }
+    this.eventBusSub = this.eventBusService.on('logout', () => {
+      this.logout(true);
+    });
+  }
+
+  logout($event:boolean): void {
+    if($event){
+      this.authService.logout().subscribe({
+        next: res => {
+          console.log(res);
+          this.storageService.clean();
+
+          window.location.reload();
+        },
+        error: err => {
+          console.log(err);
+        }
+      });
+    }
+
+  }
 }
--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { AuthService } from 'src/app/_services/auth.service';
-import { StorageService } from 'src/app/_services/storage.service';
-import { EventBusService } from 'src/app/_shared/event-bus.service';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
@@ -11,43 +8,20 @@ import { EventBusService } from 'src/app/_shared/event-bus.service';
 })
 export class HeaderComponent implements OnInit{
   private roles: string[] = [];
-  isLoggedIn = false;
-  showAdminBoard = false;
-  showModeratorBoard = false;
-  username?: string;
+  @Input()
+  username!:string;
 
-  eventBusSub?: Subscription;
+  @Input()
+  isLoggedIn!:boolean
 
-  constructor(private storageService: StorageService, private authService: AuthService,private eventBusService: EventBusService){}
+  @Output()
+  logOutEvent = new EventEmitter<boolean>
 
   ngOnInit(): void {
-    this.isLoggedIn = this.storageService.isLoggedIn();
 
-    if (this.isLoggedIn) {
-      const user = this.storageService.getUser();
-      // this.roles = user.roles;
-
-      // this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
-      // this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
-
-      this.username = user.username;
-    }
-    this.eventBusSub = this.eventBusService.on('logout', () => {
-      this.logout();
-    });
   }
 
-  logout(): void {
-    this.authService.logout().subscribe({
-      next: res => {
-        console.log(res);
-        this.storageService.clean();
-
-        window.location.reload();
-      },
-      error: err => {
-        console.log(err);
-      }
-    });
+  btnLogout(){
+    this.logOutEvent.emit(true)
   }
 }
