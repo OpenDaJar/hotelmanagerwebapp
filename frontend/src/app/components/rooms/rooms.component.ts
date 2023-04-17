780e283e6b71756f5d180eb91c045211d16b3d53
Ladas Michail <47851156+OpenDaJar@users.noreply.github.com>
2023-04-17 18:31:19 +0300

    roomlist

--- frontend/src/app/components/rooms/rooms.component.ts
+++ frontend/src/app/components/rooms/rooms.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Room } from '../../models/room.model';
 import { RoomService } from './services/room.service';
+import { MatDialog ,MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-rooms',
@@ -8,10 +9,12 @@ import { RoomService } from './services/room.service';
   styleUrls: ['./rooms.component.scss'],
 })
 export class RoomsComponent implements OnInit {
-  rooms?: Room[];
-  room?: Room = {};
-  message="";
+  rooms: Room[]=[{}];
+  room: Room = {};
+  message = '';
+  tableCols?: string[] = ['number', 'type', 'price', 'extras', 'available','edit','delete'];
 
+  // constructor(private roomService: RoomService,private dialog: MatDialog) {}
   constructor(private roomService: RoomService) {}
 
   ngOnInit() {
@@ -22,9 +25,9 @@ export class RoomsComponent implements OnInit {
     this.roomService.getAllRooms().subscribe({
       next: (data) => {
         this.rooms = data;
-        console.log(data);
+        console.log(this.rooms);
       },
-      error: (e) => console.error(e)
+      error: (e) => console.error(e),
     });
   }
 
@@ -38,28 +41,52 @@ export class RoomsComponent implements OnInit {
     });
   }
 
-  updateRoom(id:any):void{
+  updateRoom(id: any): void {
     //for testing
     // this.retrieveRoom(id)
 
-    const data={
-      number:this.room?.number,
-      type:this.room?.type,
-      price:this.room?.price,
-      extras:this.room?.extras,
-      available:this.room?.available
-    }
+    const data = {
+      number: this.room?.number,
+      type: this.room?.type,
+      price: this.room?.price,
+      extras: this.room?.extras,
+      available: this.room?.available,
+    };
     this.message = '';
 
-    this.roomService.updateRoom(id, data)
-      .subscribe({
-        next: (res) => {
-          console.log(res);
-          this.message = res.message ? res.message : 'This tutorial was updated successfully!';
-        },
-        error: (e) => console.error(e)
-      });
+    this.roomService.updateRoom(id, data).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.message = res.message
+          ? res.message
+          : 'This tutorial was updated successfully!';
+      },
+      error: (e) => console.error(e),
+    });
+  }
 
+  clickedRow(row:Room):void{
+    console.log(row);
+  }
+
+  clickedEdit(room:Room):void{
+    console.log("clicked Edit",room.id);
+    // this.openDialog();
 
   }
+
+  clickedRemove(room:Room):void{
+    console.log("clicked Remove",room.id)
+  }
+
+  // openDialog(): void {
+  //   const dialogRef = this.dialog.open(RoomDialogComponent, {
+  //     data: {name: "this.name", animal: "this.animal"},
+  //   });
+
+  //   dialogRef.afterClosed().subscribe(result => {
+  //     console.log('The dialog was closed');
+  //     // this.animal = result;
+  //   });
+  // }
 }
--- frontend/src/app/components/rooms/rooms.module.ts
+++ frontend/src/app/components/rooms/rooms.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { RoomsRoutingModule } from './rooms-routing.module';
 import { RoomsComponent } from './rooms.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
   declarations: [
-    RoomsComponent
+    RoomsComponent,
   ],
   imports: [
     CommonModule,
-    RoomsRoutingModule
+    RoomsRoutingModule,
+    MatTableModule,
+    MatDialogModule,
+    MatButtonModule,
   ]
 })
 export class RoomsModule { }
