71943ba2e2cdcb3c91726d9960c9fe80a4309edd
Ladas Michail <47851156+OpenDaJar@users.noreply.github.com>
2023-05-16 19:05:38 +0300

    update ports frontend

--- frontend/src/app/components/add-room/services/add-room.service.ts
+++ frontend/src/app/components/add-room/services/add-room.service.ts
@@ -2,7 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const API_URL = 'http://localhost:8080/api/rooms';
+// const API_URL = 'http://localhost:8080/api/rooms';
+const API_URL = 'http://localhost:6868/api/rooms';
 
 @Injectable({
   providedIn: 'root'
